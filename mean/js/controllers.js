17822efb4b004f6358a946a6e553a9a26bd00824
Monarin Uervirojnangkoorn <monarin@gmail.com>
2018-03-05 12:32:24 -0800

    remove obsolete

--- mean/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var todoControllers = angular.module('todoControllers', []);
-
-todoControllers.controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
-  $scope.todos = Todos;
-}])
-
-todoControllers.controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todos', function ($scope, $routeParams, Todos) {
-  $scope.todo = Todos[$routeParams.id];
-}])
